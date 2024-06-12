29c082b14c490ff700c3e76abf9ac57145bd8d84
Fernando de la Cruz Ramos <fcruz@centraldealarmas.com.mx>
2024-06-12 17:58:53 -0600

    props

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { useState } from "react"
 
 function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = [];
+
 
   return (
     <>
@@ -15,8 +17,13 @@ function App() {
 
             <div className="row mt-5">
               {
-                data.map((element, i) => (
-                  <Guitar key={i}/>   
+                data.map((guitar) => (
+                  <Guitar 
+                    key={guitar.id}
+                    guitar={guitar}
+                    setCart={setCart}
+                    cart={cart}
+                  />   
                   )
                 )
               }
